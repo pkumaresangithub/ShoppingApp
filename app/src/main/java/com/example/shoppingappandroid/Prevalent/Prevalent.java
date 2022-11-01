74a7e3c923351c78f58c527728134068625f40d8
pkumaresangithub <pkumaresanin@gmail.com>
2022-11-01 22:25:23 +0530

    first commit

---
+++ app/src/main/java/com/example/shoppingappandroid/Prevalent/Prevalent.java
@@ -0,0 +1,14 @@
+package com.example.shoppingappandroid.Prevalent;
+
+
+import com.example.shoppingappandroid.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    // Paper dependency
+    // save user information at the time of login
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
